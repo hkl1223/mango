689e06b1847a5bd2665fbd9b96af2b2282b0214f
hkl1223 <8465044+hkl1223@user.noreply.gitee.com>
2020-12-19 16:39:21 +0800

    创建router

---
+++ src/views/Labels.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    label
+    <Nav/>
+  </div>
+</template>
+
+<script lang ="ts">
+export default {
+    name:"Money",
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    statistics
+    <Nav />
+  </div>
+</template>
+
+<script lang ="ts">
+export default {
+  name: "Money"
+};
+</script>
+
+<style>
+</style>
