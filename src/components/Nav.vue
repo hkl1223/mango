b0bb2f67a06b9f7c740f2f4f4bac811a1bb18f47
hkl1223 <8465044+hkl1223@user.noreply.gitee.com>
2020-12-20 12:18:36 +0800

    引入icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="nav">
-      <router-link to ="/money">记账</router-link>
-      <router-link to ="/labels">标签</router-link>
-      <router-link to ="/statistics">统计</router-link>
+    <div>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-label"></use>
+      </svg>
+      <router-link to="/money">记账</router-link>
     </div>
+    <div>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-money"></use>
+      </svg>
+      <router-link to="/labels">标签</router-link>
+    </div>
+    <div>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-tongji1"></use>
+      </svg>
+      <router-link to="/statistics">统计</router-link>
+    </div>
+  </div>
 </template>
 
 <script lang ="ts">
 export default {
-    name:'Nav'
-}
+  name: "Nav",
+};
 </script>
 
 <style lang="scss" scoped>
-
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+.nav{
+  display: flex;
+}
 </style>
