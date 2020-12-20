6fda4fba31d0cc14b7759d4ea3ee0caa57925f30
hkl1223 <8465044+hkl1223@user.noreply.gitee.com>
2020-12-20 15:07:25 +0800

    修改样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="nav">
-    <div>
-      <svg class="icon" aria-hidden="true">
+  <nav>
+    <div class="item">
+      <svg class="icon">
         <use xlink:href="#icon-label"></use>
       </svg>
       <router-link to="/money">记账</router-link>
     </div>
-    <div>
-      <svg class="icon" aria-hidden="true">
+    <div class="item">
+      <svg class="icon">
         <use xlink:href="#icon-money"></use>
       </svg>
       <router-link to="/labels">标签</router-link>
     </div>
-    <div>
-      <svg class="icon" aria-hidden="true">
+    <div class="item">
+      <svg class="icon" >
         <use xlink:href="#icon-tongji1"></use>
       </svg>
       <router-link to="/statistics">统计</router-link>
     </div>
-  </div>
+  </nav>
 </template>
 
 <script lang ="ts">
@@ -35,7 +35,17 @@ export default {
   fill: currentColor;
   overflow: hidden;
 }
-.nav{
+nav {
   display: flex;
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  flex-direction: row;
+
+> .item {
+  width: 33.33333%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  padding: 2 0px;
+  }
 }
 </style>
