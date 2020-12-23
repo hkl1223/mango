5ef32a4c3db568db32df6211ff7fd759c7fe0c10
hkl1223 <1746530669@qq.com>
2020-12-23 20:17:10 +0800

    开始搭建label页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
     <Layout>
-      Labels
+      <ol>
+        <li>
+          <span>衣</span>
+          <Icon name="right"/>
+        </li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ol>
+      <div>
+        <button>新建标签</button>
+      </div>
     </Layout>
-    </div>
+  </div>
 </template>
 
 <script lang ="ts">
+import Icon from '@/components/Icon.vue';
 export default {
-    name:"Labels",
-}
+  components: { Icon },
+  name: "Labels",
+};
 </script> 
