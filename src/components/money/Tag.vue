467e65e5b05cc6c11ade67924545bf0b59775efd
hkl1223 <1746530669@qq.com>
2020-12-24 19:40:52 +0800

    消除tag警告

--- src/components/money/Tag.vue
+++ src/components/money/Tag.vue
@@ -6,11 +6,10 @@
     <ul class="current">
       <li
         v-for="tag in dataSource"
-        :key="tag"
+        :key="tag.id"
         :class="{ selected: selectedTags.indexOf(tag) >= 0 }"
-        @click="toggle(tag)"
-      >
-        {{ tag }}
+        @click="toggle(tag)">
+        {{ tag.name }}
       </li>
     </ul>
   </div>
