c075dd238abfd30aea007f1191a20db1d7222ded
hkl1223 <1746530669@qq.com>
2020-12-25 10:55:40 +0800

    id生成

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,5 @@
+import creatId from "@/lib/creatId";
+
 const localStorageKeyName = 'recordList'
 type Tag = {
     id: string;
@@ -19,9 +21,11 @@ const tagListModel: TagListModel = {
         return this.data;
     },
     create(name) {
+
         const names = this.data.map(item => item.name)
         if (names.indexOf(name) >= 0) { return 'duplicated'; }
-        this.data.push({ id: name, name: name });
+        const id = creatId().toString()
+        this.data.push({ id, name: name });
         this.save();
         return 'success';
     },
