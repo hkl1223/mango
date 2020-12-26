b561ab41584efd69ace659028c2e9acf633f0e78
hkl1223 <1746530669@qq.com>
2020-12-26 14:03:33 +0800

    再优化

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,30 +1,9 @@
-import tagListModel from '@/models/tagListModel'
-import recordListModel from '@/models/recordListModel'
+import recordStore from "./recordStore";
+import tagStore from "./tagStore";
+
 const store = {
-    //record stroe
-    recordList: recordListModel.fetch(),
-    createRecord: (record: RecordItem) => recordListModel.create(record),
-  
-    //tag store
-    tagList: tagListModel.fetch(),
-    findTag(id: string) {
-      return this.tagList.filter((t) => t.id === id)[0];
-  
-    },
-    crateTag: (name: string) => {
-      const message = tagListModel.create(name);
-      if (message === "duplicated") {
-        window.alert("标签名重复了");
-      } else if (message === "success") {
-        window.alert("添加成功");
-      }
-    },
-    removeTag: (id: string) => {
-      return tagListModel.remove(id);
-  
-    },
-    updateTag: (id: string, name: string) => {
-      return tagListModel.update(id, name);
-    }
-  }
-  export default store;
+    ...recordStore,
+    ...tagStore,
+
+};
+export default store;
