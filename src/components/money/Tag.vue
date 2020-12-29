1699c198a8e4299f81f22eddcb8a3d52072c102b
hkl1223 <1746530669@qq.com>
2020-12-29 18:40:30 +0800

    使用 tagHelper 作为 minixs

--- src/components/money/Tag.vue
+++ src/components/money/Tag.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button @click="create">新增标签</button>
+      <button @click="createTag">新增标签</button>
     </div>
     <ul class="current">
       <li
@@ -17,35 +17,32 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import {Component, Prop} from 'vue-property-decorator';
-  @Component({
-    computed: {
-      tagList() {
-        return this.$store.state.tagList;
-      }
-    }
-  })
-  export default class Tags extends Vue {
-    selectedTags: string[] = [];
-    created() {
-      this.$store.commit('fetchTags');
-    }
-    toggle(tag: string) {
-      const index = this.selectedTags.indexOf(tag);
-      if (index >= 0) {
-        this.selectedTags.splice(index, 1);
-      } else {
-        this.selectedTags.push(tag);
-      }
-      this.$emit('update:value', this.selectedTags);
-    }
-    create() {
-      const name = window.prompt('请输入标签名');
-      if (!name) { return window.alert('标签名不能为空'); }
-      this.$store.commit('createTag', name);
-    }
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import mixins from "vue-class-component";
+import TagHelper from "@/minixs/tagHelper";
+@Component({
+  computed: {
+    tagList() {
+      return this.$store.state.tagList;
+    },
+  },
+})
+export default class Tags extends mixins(TagHelper) {
+  selectedTags: string[] = [];
+  created() {
+    this.$store.commit("fetchTags");
   }
+  toggle(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
+    }
+    this.$emit("update:value", this.selectedTags);
+  }
+}
 </script>
 
 <style lang="scss" scoped>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -24,17 +24,27 @@
 <script lang ="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
-import store from "@/store/index2";
+import mixins from "vue-class-component";
+import Button from "@/components/Button.vue";
+import TagHelper from "@/minixs/tagHelper";
 
-@Component
-export default class Lables extends Vue {
-  tags = store.tagList;
-
+@Component({
+  computed: {
+    tags() {
+      return this.$store.state.tagList;
+    },
+  },
+})
+export default class Lables extends mixins(TagHelper) {
+  beforeCreate() {
+    this.$store.commit("fetchTags");
+  }
   createTag() {
     const name = window.prompt("请输入标签名");
-    if (name) {
-      store.createTag(name);
+    if (!name) {
+      return window.alert("标签名不能为空");
     }
+    this.$store.commit("createTag", name);
   }
 }
 </script> 
