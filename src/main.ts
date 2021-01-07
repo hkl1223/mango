814c2e6fca42f4f7c0d4eb6226af684aa0a1109c
hkl1223 <1746530669@qq.com>
2021-01-07 18:28:11 +0800

    再次优化

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,6 @@ window.onload = function () {
   }, 0)
 }
 
-if(document.documentElement.clientWidth>500){
+if (document.documentElement.clientWidth > 500) {
   window.alert('请使用手机打开本页面，以保证浏览效果')
 }
